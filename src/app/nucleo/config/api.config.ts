3cb182c6f281da17023084d0cebf2deeead109f9
unknown <ritherman03@gmail.com>
2026-02-08 11:18:37 -0500

    actualizacion6

--- src/app/nucleo/config/api.config.ts
+++ src/app/nucleo/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
   window.location.hostname === 'localhost'
     ? 'http://localhost:3000'
-    : 'https://registro-asistencia-api-04yz.onrender.com';
+    : 'https://registro-asistencia-api-o4yz.onrender.com';
